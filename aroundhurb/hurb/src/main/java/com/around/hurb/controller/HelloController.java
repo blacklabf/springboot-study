4f8f83a5e80b2a2c9af360eec3ebe3b8eafda3c0
방소영 <bjo8244@naver.com>
2024-02-20 09:54:01 +0900

    [Feat] HelloController 작성
    RestController 기초 실습

--- aroundhurb/hurb/src/main/java/com/around/hurb/controller/HelloController.java
+++ aroundhurb/hurb/src/main/java/com/around/hurb/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.around.hurb.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping
+    // @RequestMapping(value = "/hello") - method 없이 설정 : 어떠한 method로 보내도 같은 응답
+    // @RequestMapping(value="/hello, method = RequestMethod.GET) - 고전방식
+    @GetMapping("hello")
     public String hello(){
         return "Hello World";
     }
